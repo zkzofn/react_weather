a31f5a65ffd9ad5665bfe80a6e97cee1aa5e6f43
zkzofn <zkzofn@gmail.com>
2016-07-11 01:43:46 +0900

    Add png file

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,12 +2,12 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
   switch (action.type) {
-  case FETCH_WEATHER:
-    // 여기서 state.push 를 할 경우 state mutation 이 일어날 수 있다.
-    // concat 을 사용하면 mutation 을 방지할 수 있다.
-    // return state.concat(action.payload.data);
-    // --> return state.concat([action.payload.data]);
-    return [ action.payload.data, ...state ]; // [ city, city, city ] NOT [ city, [ city, city ] ]
+    case FETCH_WEATHER:
+      // 여기서 state.push 를 할 경우 state mutation 이 일어날 수 있다.
+      // concat 을 사용하면 mutation 을 방지할 수 있다.
+      // return state.push(action.payload.data);
+      // --> return state.concat([action.payload.data]);
+      return [ action.payload.data, ...state ]; // [ city, city, city ] NOT [ city, [ city, city ] ]
   }
   return state;
 }
